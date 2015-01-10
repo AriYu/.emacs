4c372cb11893d08a47c785c08da7da57e49f1607
有田 裕太 <tafuyuu@arita-yuuta-no-iMac.local>
2015-01-11 01:59:49 +0900

    fix - for Mac OS 10.9.5

--- init.el
+++ init.el
@@ -105,7 +105,8 @@
  ;; If there is more than one, they won't work right.
  '(show-paren-mode t)
  '(tab-width 4)
- '(tool-bar-mode nil))
+ ;; '(tool-bar-mode nil)
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -134,30 +135,30 @@
 
 
 ;; inverse search
-(require 'dbus)
+;; (require 'dbus)
 
-(defun un-urlify (fname-or-url)
-  "A trivial function that replaces a prefix of file:/// with just /."
-  (if (string= (substring fname-or-url 0 8) "file:///")
-      (substring fname-or-url 7)
-    fname-or-url))
+;; (defun un-urlify (fname-or-url)
+;;   "A trivial function that replaces a prefix of file:/// with just /."
+;;   (if (string= (substring fname-or-url 0 8) "file:///")
+;;       (substring fname-or-url 7)
+;;     fname-or-url))
 
-(defun evince-inverse-search (file linecol &rest ignored)
-  (let* ((fname (un-urlify file))
-         (buf (find-file fname))
-         (line (car linecol))
-         (col (cadr linecol)))
-    (if (null buf)
-        (message "[Synctex]: %s is not opened..." fname)
-      (switch-to-buffer buf)
-      (goto-line (car linecol))
-      (unless (= col -1)
-        (move-to-column col)))))
+;; (defun evince-inverse-search (file linecol &rest ignored)
+;;   (let* ((fname (un-urlify file))
+;;          (buf (find-file fname))
+;;          (line (car linecol))
+;;          (col (cadr linecol)))
+;;     (if (null buf)
+;;         (message "[Synctex]: %s is not opened..." fname)
+;;       (switch-to-buffer buf)
+;;       (goto-line (car linecol))
+;;       (unless (= col -1)
+;;         (move-to-column col)))))
 
-(dbus-register-signal
- :session nil "/org/gnome/evince/Window/0"
- "org.gnome.evince.Window" "SyncSource"
- 'evince-inverse-search)
+;; (dbus-register-signal
+;;  :session nil "/org/gnome/evince/Window/0"
+;;  "org.gnome.evince.Window" "SyncSource"
+;;  'evince-inverse-search)
 
 ;; auto-install
 

1fb99fca91da57a042a54b495e83926b247af166
Arita Yuta <k104009y@mail.kyutech.jp>
2015-01-12 06:34:26 +0900

    追加 - markdown-modeを追加
    修正
    - ツールバーを非表示に
    
    その他
    - w3mを入れようとしたら入らなかった

--- init.el
+++ init.el
@@ -102,19 +102,20 @@
 (setq mouse-wheel-progressive-speed nil) ;;; dont accelerate scrolling
 (setq mouse-wheel-follow-mouse 't) ;;; scroll window under mouse
 (custom-set-variables
-;;; custom-set-variables was added by Custom.
-;;; If you edit it by hand, you could mess it up, so be careful.
-;;; Your init file should contain only one such instance.
-;;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(flycheck-display-errors-function (function flycheck-pos-tip-error-messages))
  '(show-paren-mode t)
  '(tab-width 4)
-;;; '(tool-bar-mode nil)
- )
+ '(tool-bar-mode nil)
+ '(tool-bar-position (quote bottom)))
 (custom-set-faces
-;;; custom-set-faces was added by Custom.
-;;; If you edit it by hand, you could mess it up, so be careful.
-;;; Your init file should contain only one such instance.
-;;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
 
 ;;;YaTexの設定

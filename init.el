4aa3dfc608b1cf67b867aaad1152ccba52d19c3a
AriYu-Lab <k104009y@mail.kyutech.jp>
2015-03-05 22:37:36 +0900

    [Add] tramp, web-mode

--- init.el
+++ init.el
@@ -291,3 +291,20 @@
 (pdf-tools-install)
 ;;(setq revert-without-query 'yes)
 (add-hook 'doc-view-mode-hook 'auto-revert-mode)
+
+;; for ssh
+(require 'tramp)
+(setq tramp-default-method "scp")
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(setq web-mode-engines-alist
+'(("php"    . "\\.phtml\\'")
+  ("blade"  . "\\.blade\\.")))

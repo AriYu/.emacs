2ebd3535fcc459a95e9041ebc6df0a28fbf6a155
Arita Yuta <k104009y@mail.kyutech.jp>
2015-03-16 23:10:06 +0900

    [Add] add key bind rules for magit status

--- init.el
+++ init.el
@@ -310,5 +310,15 @@
 '(("php"    . "\\.phtml\\'")
   ("blade"  . "\\.blade\\.")))
 (setq web-mode-enable-current-element-highlight t)
+(defun my-web-mode-hook () "Hooks for Web mode." 
+  (setq web-mode-markup-indent-offset 2) 
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  ) 
+(add-hook 'web-mode-hook 'my-web-mode-hook)
 ;;;(setq web-mode-enable-current-column-highlight t)
 ;;(setq web-mode-current-element-highlight-face "#0000cd")
+
+;; magit
+(global-set-key (kbd "C-x g") 'magit-status)

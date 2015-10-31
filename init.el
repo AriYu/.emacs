0ef4789b318e640502b86b68562c42c6373c8ecd
Arita Yuta <k104009y@mail.kyutech.jp>
2015-10-31 22:38:40 +0900

    [add] google-thisを追加

--- init.el
+++ init.el
@@ -464,3 +464,11 @@
 ;;                                      TeX-run-discard-or-function t t :help "Forward search with Evince"))
 ;;                       )))
 
+(setq google-this-keybind (kbd "C-<f1>"))
+(google-this-mode 1)
+(require 'google-this)
+(setq google-this-location-fix "co.jp")
+(defun google-this-url () "URL for google searches."
+  ;; 100件/日本語ページ/5年以内ならこのように設定する
+  (concat google-this-base-url google-this-location-suffix
+          "/search?q=%s&hl=ja&num=100&as_qdr=y5&lr=lang_ja"))
